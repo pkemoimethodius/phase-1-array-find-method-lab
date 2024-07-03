95ac707bade9d7dfc9bcb9ab1f24fcb5d8536932
methodius pkemoi <pkemoimethodius@gmail.com>
2024-07-03 14:33:33 +0300

    Adds the filter method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array){
+    const win = array.find(function(element){
+      return element.result === "W"
+    })
+    if(win){
+      return win.year
+    }
+    return undefined
+  }
